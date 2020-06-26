02a59336228cca5ccb2ec93e435985d7fe055d3e
Wuif <5678318+wuifdesign@users.noreply.github.com>
2020-06-26 21:19:24 +0200

    updated package versions, removed bower and moved to npm

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,23 @@
 'use strict';
 
-var gulp    = require('gulp');
-var plugins = require('gulp-load-plugins')();
+const gulp = require('gulp');
+const plugins = require('gulp-load-plugins')();
 
-var config = {
-    getTask: getTask,
-    srcPath: './resources/assets', //change to your source path
-    publicPath: './docs/assets' //change to your target path
+const getTask = (task, extra) => {
+  return require(config.srcPath + '/../gulp-tasks/' + task)(gulp, plugins, config, extra);
 };
 
-function getTask(task, extra) {
-    return require(config.srcPath + '/../gulp-tasks/' + task)(gulp, plugins, config, extra);
-}
+const config = {
+  getTask: getTask,
+  srcPath: './resources/assets', //change to your source path
+  publicPath: './docs/assets', //change to your target path
+};
 
+gulp.task('symlink', getTask('symlink'));
 gulp.task('copy-files', getTask('copy-files'));
 gulp.task('sass', getTask('sass'));
 gulp.task('js', getTask('js'));
 gulp.task('js-vendor', getTask('js-vendor'));
 gulp.task('watch', getTask('watch'));
 
-//Generates Sprite images and adds the image to the "sprites_path" and the SCSS-files to "sass/custom/sprites"
-gulp.task('sprite', getTask('sprite', {
-    enable_retina_sprites: false, //Enable retina sprites which are defined as "filename@2x.png" and have to be exactly double the size of the normal image
-    sprites_path: config.publicPath + '/img/sprites/', //Path to the spite images
-    relative_sprites_path: '../img/sprites/', //Relative path to the spite images (used in the css after compiling)
-    sprites: [ /* 'base' */ ] //add folder names of the sprites. (if folder is "../img/sprites/base" add "base" to the array
-}));
-
-gulp.task('default', ['copy-files', 'sass', 'js-vendor', 'js']);
+gulp.task('default', gulp.parallel('copy-files', 'sass', 'js-vendor', 'js'));
