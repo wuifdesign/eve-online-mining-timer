e63306cea4f9b744decad22ca70c365e1c0db692
Zeereen <18538136+Zeereen@users.noreply.github.com>
2026-02-02 18:47:53 -0600

    Added Ryuuki Ran's contributor name to project

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react'
-import { Box, Text } from '@mantine/core'
+import { Box, Text, Tooltip } from '@mantine/core'
+import { AiOutlineInfoCircle } from 'react-icons/ai'
 
 export const Footer: React.FC = () => {
   return (
     <Box mt="xl" mb="md" ta="center">
       <Text>
         Tool created by <b>Marumis Wuif</b>
+      </Text>
+      <Text>
+        With the help of{' '}
+        <Tooltip label="For the Nov 2025 mining mechanics updates (Catalyst expansion)" withArrow>
+          <Text span fw={700} style={{ display: 'inline-flex', alignItems: 'center', gap: 4, cursor: 'help' }}>
+            Ryuuki Ran <AiOutlineInfoCircle aria-hidden="true" size={14} />
+          </Text>
+        </Tooltip>
       </Text>
       <Text c="dimmed" size="sm">
         Feel free to send me questions, suggestions or donations of ISK in game.
